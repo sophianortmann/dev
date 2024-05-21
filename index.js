dc4b437e7707bfaad598a27284d6a4aafe43e87e
sophianortmann <sophia.nortmann@gmx.de>
2024-05-21 22:03:49 +0200

    new Alert

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 document.querySelector("#vielleicht").addEventListener("click", () => alert("Du spinnst wohl!"))
-document.querySelector("#nein").addEventListener("click", () => alert("Überleg's dir nochmal!"))
+document.querySelector("#nein").addEventListener("click", () => alert("Überleg nochmal!"))
+document.querySelector("#ja").addEventListener("click", () => alert("Donnerstag 19 Uhr?"))
