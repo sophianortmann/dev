a50a6a3daeba67c36c79b3c17f79ef0b4281e2fb
sophianortmann <sophia.nortmann@gmx.de>
2024-05-21 21:18:39 +0200

    js Dingens

---
+++ index.js
@@ -0,0 +1,2 @@
+document.querySelector("#vielleicht").addEventListener("click", () => alert("Du spinnst wohl!"))
+document.querySelector("#nein").addEventListener("click", () => alert("Überleg's dir nochmal!"))
